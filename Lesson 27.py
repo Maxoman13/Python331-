f0b7ecae3581c45e3c26ffa0915e0608991f0bd8
Maxoman13 <maxxorosh@mail.ru>
2023-12-05 20:48:47 +0300

    Small Matryoshka

--- Lesson 27.py
+++ Lesson 27.py
@@ -3,7 +3,7 @@ class BigMatryoshka:
         self.size = 'Big'
 
     def open(self):
-        print(f"Opening the big matryoshka")
+        print("Opening the big matryoshka")
 
 
 class MediumMatryoshka(BigMatryoshka):
@@ -12,12 +12,22 @@ class MediumMatryoshka(BigMatryoshka):
         self.color = color
 
     def open(self):
-        print(f"Opening the medium matryoshka")
+        print("Opening the medium matryoshka")
         super().open()
 
     def display_info(self):
-        print(f"Opening the {self.size} {self.color} matryoshka")
+        print(f"Opening the medium {self.color} matryoshka")
 
 
-user = MediumMatryoshka('Red')
+class SmallMatryoshka(MediumMatryoshka):
+    def __init__(self, color):
+        super().__init__(color)
+        self.size = 'small'
+
+    def open(self):
+        super().open()
+        print("Opening the small matryoshka")
+
+
+user = SmallMatryoshka('red')
 user.open()
