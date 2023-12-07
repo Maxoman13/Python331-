a37474fd8340d6ccb770de63ffda78c8f0efcf00
Maxoman13 <maxxorosh@mail.ru>
2023-12-07 21:57:44 +0300

    Homework 16 txt разбор

--- Homework 16.py
+++ Homework 16.py
@@ -60,25 +60,25 @@ class CsvFileHandler:
 # user_file.read_file('C:/Users/User/Desktop/Python/Python331-/привет.json')
 
 
-# class TxtFileHandler:
-#
-#     @staticmethod
-#     def read_file(filepath):
-#         with open(f'{filepath}', 'r', encoding='UTF-8', newline='') as f:
-#             file = f.read()
-#             print(file)
-#
-#     @staticmethod
-#     def write_file(filepath, data):
-#         with open(f'{filepath}', 'w', encoding='UTF-8', newline='') as f:
-#             file = f.write(data+'\n')
-#             return file
-#
-#     @staticmethod
-#     def append_file(filepath, data):
-#         with open(f'{filepath}', 'a', encoding='UTF-8', newline='') as f:
-#             file = f.write(data+'\n')
-#             return file
+class TxtFileHandler:
+
+    @staticmethod
+    def read_file(filepath):
+        with open(f'{filepath}', 'r', encoding='UTF-8', newline='') as f:
+            file = f.read()
+            print(file)
+
+    @staticmethod
+    def write_file(filepath, data):
+        with open(f'{filepath}', 'w', encoding='UTF-8', newline='') as f:
+            file = f.write(data+'\n')
+            return file
+
+    @staticmethod
+    def append_file(filepath, data):
+        with open(f'{filepath}', 'a', encoding='UTF-8', newline='') as f:
+            file = f.write(data+'\n')
+            return file
 #
 #
 # user_file = TxtFileHandler()
---
+++ Homework 17.py
@@ -0,0 +1,18 @@
+class SmartHome:
+    def __init__(self, name: str):
+        self.name = name
+
+    def switch_on(self):
+        return f'{self.name} включено'
+
+    def switch_off(self):
+        return f'{self.name} выключено'
+
+    def battery(self):
+        return f'{self.name} в процессе работы'
+
+
+user = SmartHome('Maksim house')
+print(user.switch_on())
+print(user.switch_off())
+print(user.battery())
